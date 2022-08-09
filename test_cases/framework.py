6a66db2009ef05e667f1994abdd0a5f357bb626f
mvs1994 <32595716+mvs1994@users.noreply.github.com>
2022-08-09 11:12:51 +0200

    first_task

--- test_cases/framework.py
+++ test_cases/framework.py
@@ -6,17 +6,19 @@ from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
 
 class Test(unittest.TestCase):
 
-    @classmethod
-    def setUp(self):
-        os.chmod(DRIVER_PATH, 755)
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-        self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
-        self.driver.fullscreen_window()
-        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+    driver = None
 
     @classmethod
-    def tearDown(self):
-        self.driver.quit()
+    def setUp(cls):
+        os.chmod(DRIVER_PATH, 755)
+        cls.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        cls.driver.get('https://scouts-test.futbolkolektyw.pl/en')
+        cls.driver.fullscreen_window()
+        cls.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    @classmethod
+    def tearDown(cls):
+        cls.driver.quit()
 
     def test_print_nice_words(self):
         print("WELL DONE!!!!!!!!!")
